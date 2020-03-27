c3638e06c8469e1437421a669ca7f13697f8e164
Rafael Carvalho <ytayfoorx@gmail.com>
2020-03-27 20:07:06 -0300

    Testes BackEnd

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const routes = require('./routes');
-const cors = require('cors');
 
+const express = require("express");
+const cors = require("cors");
+const { errors } = require("celebrate");
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
+module.exports = app;
 /**
  * Métodos HTTP:
  * 
@@ -27,5 +30,3 @@ app.use(routes);
  */
 
 
-
-app.listen(3333);
--- Backend/src/controllers/OngController.js
+++ Backend/src/controllers/OngController.js
@@ -1,19 +1,27 @@
-const connection = require('../database/connection');
-const crypto = require('crypto');
+const crypto = require("crypto");
+
+
+const connection = require("../database/connection");
 
 module.exports = {
-    async index(request, response) {
-        const ongs = await connection('ongs').select('*');
+    async index (request, response) {
+        const ongs = await connection("ongs").select("*");
     
         return response.json(ongs);
     },
-
+    
     async create(request, response) {
-        const { name , email, whatsapp, city, uf } = request.body;
+        const {
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        } = request.body;
 
         const id = crypto.randomBytes(4).toString('HEX');
-        
-        await connection('ongs').insert({
+
+        await connection("ongs").insert({
             id,
             name,
             email,
@@ -21,7 +29,10 @@ module.exports = {
             city,
             uf,
         })
-    
-        return response.json({ id });
+
+        return response.json({
+            id
+        });
+
     }
-};
+}
